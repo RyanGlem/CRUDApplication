3e8bb9bec3b5ea3f01388349ddfac36c264667e3
Sam Kessler <sholomkessler1@gmail.com>
2021-01-21 17:07:53 -0500

    Added route for single campus

--- routes/campuses.js
+++ routes/campuses.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 const { Campus } = require('../database/models');
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
 	Campus.findAll()
 		.then((students) => res.json(students))
 		.catch((err) => console.error(err));
+});
+
+router.get('/:id', (req, res, next) => {
+	Campus.findByPk(req.params.id)
+		.then((campus) => {
+			campus
+				.getStudents()
+				.then((students) => {
+					res.json({ campus, students });
+				})
+				.catch((err) => {
+					res.json({ campus });
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 module.exports = router;
--- utilities/seedDatabase.js
+++ utilities/seedDatabase.js
@@ -3,19 +3,41 @@ const { Student, Campus } = require('../database/models');
 const seedDatabase = async () => {
 	try {
 		await Promise.all([
-			Student.create({
-				firstName: 'Kyrie',
-				lastName: 'Irving',
-				email: 'foo@bar.com',
-				imageUrl: 'https://scx2.b-cdn.net/gfx/news/2018/83-researchersi.jpg',
-				gpa: 3.0,
-			}),
 			Campus.create({
 				name: 'Brooklyn College',
 				imageUrl: 'https://www.brooklyn.cuny.edu/web/com_socialImages/BrooklynCollegeLibrary_1200x628.jpg',
 				address: '2900 Bedford Ave, Brooklyn, NY 11210',
 				description:
 					'Brooklyn College is a public college in Brooklyn, New York. It is part of the City University of New York and enrolls about 15,000 undergraduate and 2,800 graduate students on a 35-acre campus.',
+			}),
+			Student.create({
+				firstName: 'Kyrie',
+				lastName: 'Irving',
+				email: 'foo@bar.com',
+				imageUrl: 'https://scx2.b-cdn.net/gfx/news/2018/83-researchersi.jpg',
+				gpa: 3.0,
+				campusId: 1,
+			}),
+			Student.create({
+				firstName: 'Daniel',
+				lastName: 'Moore',
+				email: 'foo@baz.com',
+				gpa: 3.4,
+				campusId: 1,
+			}),
+			Student.create({
+				firstName: 'Matthew',
+				lastName: 'Scott',
+				email: 'bar@baz.com',
+				gpa: 2.7,
+				campusId: 1,
+			}),
+			Student.create({
+				firstName: 'Joseph',
+				lastName: 'Taylor',
+				email: 'baz@bar.com',
+				gpa: 3.9,
+				campusId: 1,
 			}),
 		]);
 	} catch (error) {
