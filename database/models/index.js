faea32708c729cb1bae2a7d6586c5605f7ecf236
Sam Kessler <sholomkessler1@gmail.com>
2021-01-21 16:34:53 -0500

    Created associations

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,11 @@
-// Here, we can prepare to register our models, set up associations between tables, and generate a barrel file for the models;
+// Here, we can prepare to register our models,
+//set up associations between tables,
+//and generate a barrel file for the models;
 
 const Student = require('./students');
 const Campus = require('./campuses');
+
+Campus.hasMany(Student, { as: 'Students' });
 
 module.exports = {
 	Student,
