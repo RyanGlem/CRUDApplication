535e564a5a472c21a51e8c61a654fbc8f7aaf87a
Sam Kessler <sholomkessler1@gmail.com>
2021-01-21 18:30:48 -0500

    Added find single student route

--- routes/students.js
+++ routes/students.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { Student } = require('../database/models');
+const { Student, Campus } = require('../database/models');
 
 router.get('/', function (req, res, next) {
 	Student.findAll()
 		.then((students) => res.json(students))
 		.catch((err) => console.log(err));
+});
+
+router.get('/:id', (req, res, next) => {
+	Student.findByPk(req.params.id)
+		.then((student) => {
+			Campus.findByPk(student.campusId)
+				.then((campus) => {
+					res.json({ student, campus });
+				})
+				.catch((err) => {
+					res.json({ student });
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 // Export our router, so that it can be imported to construct our apiRouter;
